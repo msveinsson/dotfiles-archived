45b3d2a4db680a672981f95c2bb2f1b2695d8b76
TheM4gnus <the.m4gnus@gmail.com>
2023-10-29 18:46:46 +0000

    feat: install script to symlink using Gnu Stow dotfiles

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+DOT_FOLDERS="alacritty,bash,bat,git,htop,kitty,lf,neofetch,starship,yabai,zsh"
+
+echo "[+] Dotfiles :: $SCRIPT_DIR"
+
+if ! command -v stow &> /dev/null; then
+    brew install stow
+fi
+
+for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
+    echo "[+] Folder :: $folder"
+    #stow -t $HOME -D $folder \
+    #    --ignore=README.md --ignore=LICENSE
+    stow -t $HOME $folder
+done
