ebe8f943e46872b097c3f38885b9ec2a86852df2
TheM4gnus <the.m4gnus@gmail.com>
2023-11-05 19:13:43 +0000

    feat: update list, add skhd

--- install.sh
+++ install.sh
@@ -3,7 +3,26 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-DOT_FOLDERS="alacritty,bash,bat,fish,gh-dash,git,htop,kitty,lf,neofetch,powershell,starship,tmux,yabai,zsh"
+DOT_FOLDERS=(
+    "alacritty"
+    "bash"
+    "bat"
+    "bin"
+    "fish"
+    "gh-dash"
+    "git"
+    "htop"
+    "kitty"
+    "lf"
+    "neofetch"
+    "powershell"
+    "skhd"
+    "sketchybar"
+    "starship"
+    "tmux"
+    "yabai"
+    "zsh"
+)
 
 echo "[+] Dotfiles :: $SCRIPT_DIR"
 
@@ -11,9 +30,7 @@ if ! command -v stow &> /dev/null; then
     brew install stow
 fi
 
-for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
-    echo "[+] Folder :: $folder"
-    #stow -t $HOME -D $folder \
-    #    --ignore=README.md --ignore=LICENSE
-    stow -t $HOME $folder
+for folder in ${DOT_FOLDERS[@]}; do
+  echo "[+] Folder :: $folder"
+  #stow -t $HOME $folder
 done
